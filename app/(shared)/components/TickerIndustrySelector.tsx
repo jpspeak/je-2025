f0707075c55280c716fc2c1bafaaebc579b0fdd6
jay philip <jayphilipcamillo@gmail.com>
2025-01-06 14:54:24 +0800

    .

--- app/(shared)/components/TickerIndustrySelector.tsx
+++ app/(shared)/components/TickerIndustrySelector.tsx
@@ -14,6 +14,7 @@ function TickerIndustrySelector({
   options,
   placeholder = "Choose your industry",
 }: TickerIndustrySelectorProps) {
+  console.log(options);
   const [isOpen, setIsOpen] = useState(false);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
